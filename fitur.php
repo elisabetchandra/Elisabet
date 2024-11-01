4146a3cfe0923053d329e6cdc844376724cbb748
Richi-gif <150431744+Richi-gif@users.noreply.github.com>
2024-11-02 02:28:16 +0800

    Create fitur.php

---
+++ fitur.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Image with Description</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="fitur.css">
+    </head>
+    <body>
+        <?php
+            include ('menu.php');
+        ?>
+     <div class="Acontainer">
+        <div class="Acard">
+            <img src="OP1.webp" alt="Recycling water">
+            <div class="card-content">
+                <h3>Recycling water</h3>
+                <p>Recycling water for reuse applications instead of using freshwater supplies can be a water-saving measure.</p>
+            </div>
+        </div>
+
+        <div class="Acard">
+            <img src="Thumbnailmain.png" alt="Habitat Model">
+            <div class="card-content">
+                <h3>Habitat Model</h3>
+                <p>Habitats with a minimum footprint on the planet and a maximum positive impact on the local community.</p>
+            </div>
+        </div>
+
+        <div class="Acard">
+            <img src="Thumbnailmain.png" alt="Organic Garden">
+            <div class="card-content">
+                <h3>Organic Garden</h3>
+                <p>Learn how to use organic gardening methods to grow the freshest food in your fruit and vegetable garden.</p>
+            </div>
+        </div>
+
+        <div class="Acard">
+            <img src="Thumbnailmain.png" alt="ITatau">
+            <div class="card-content">
+                <h3>Ini Contoh</h3>
+                <p>Bla bla bla bla bla</p>
+            </div>
+        </div>
+</body>
+</html>
