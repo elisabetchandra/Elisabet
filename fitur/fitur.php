8b957b0fd8eca3504974e402b3bd69e30ad2d592
Richi-gif <150431744+Richi-gif@users.noreply.github.com>
2024-11-03 02:01:38 +0800

    fitur.php
    php dari fitur

---
+++ fitur/fitur.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Image with Description</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="fitur.css">
+    </head>
+    <body>
+        <?php
+            include ('menu.php');
+        ?>
+     <div class="Acontainer">
+        <div class="Acard">
+            <img src="OP1.webp" alt="Recycling water">
+            <div class="card-content">
+                <h3>Operation</h3>
+                <p>Fitur ini membantu mengelola aktivitas sehari-hari di gudang, seperti menerima, menyimpan, memindahkan, dan mengeluarkan barang. Ini juga mencakup pengelolaan stok dan memastikan barang tersedia sesuai kebutuhan.</p>
+            </div>
+        </div>
+
+        <div class="Acard">
+            <img src="P1.jpg" alt="Habitat Model">
+            <div class="card-content">
+                <h3>Products</h3>
+                <p>Fitur ini mencatat semua barang yang ada di gudang. Informasi detail seperti deskripsi, jumlah stok, lokasi penyimpanan, dan kategori barang (misalnya makanan atau perlengkapan) dapat diakses dan diatur di sini.</p>
+            </div>
+        </div>
+
+        <div class="Acard">
+            <img src="reporting1.png" alt="Organic Garden">
+            <div class="card-content">
+                <h3>Reporting</h3>
+                <p>Fitur ini menyediakan laporan terkait stok barang, pergerakan barang, dan penggunaan di berbagai area. Pengguna dapat membuat laporan untuk memantau stok, riwayat transaksi, dan pola penggunaan barang guna membantu dalam pengambilan keputusan.</p>
+            </div>
+        </div>
+
+        <div class="Acard">
+            <img src="help.png" alt="ITatau">
+            <div class="card-content">
+                <h3>Help</h3>
+                <p>Fitur ini memberikan bantuan atau panduan bagi pengguna. Pengguna bisa menemukan instruksi penggunaan aplikasi, jawaban atas pertanyaan umum, atau menghubungi dukungan teknis jika ada masalah dalam pengelolaan gudang.</p>
+            </div>
+        </div>
+</body>
+</html>
