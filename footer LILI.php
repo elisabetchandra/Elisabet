2628d7c62b180f63aec0f9692dd56e589b6d9d57
elisabetchandra <20231027@student.itk.ac.id>
2024-11-04 06:46:29 +0800

    Create footer LILI.php

---
+++ footer LILI.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Profil Website Layout</title>
+        <link rel="stylesheet" type="text/css" href="footer.css">
+    </head>
+    <body>
+<div class="footer">
+    <div class="footer-section">
+        <h3>How can we help?</h3>
+        <p>Contact us anytime</p>
+    </div>
+    <div class="footer-section">
+        <h3>Call us</h3>
+        <p>0812-5662-9623</p>
+    </div>
+    <div class="footer-section">
+        <h3>Send us a message</h3>
+        <p>Tracklyn18@gmail.com</p>
+    </div>
+</div>
+    </body>
+</html>
