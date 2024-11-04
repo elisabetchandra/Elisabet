c944ee2a1905db236e5ec80bb1d5879e454d1c52
elisabetchandra <20231027@student.itk.ac.id>
2024-11-04 14:23:38 +0800

    Create latar_belakang LILI.css

---
+++ latar_belakang LILI.css
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tracklyn</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scaled=1.0">
+        <link rel="stylesheet" type="text/css" href="uts.css">
+</head>
+<body>
+<?php
+         include ('menu.php');
+        ?>
+    <div class="card-container">
+        <div class="card">
+            <img src="warehousing gudang.jpg" height="100" width="200">
+            <div class="card-content">
+                <h1>Tracklyn</h1>
+                <p>Website Tracklyn hadir sebagai solusi inovatif untuk mengatasi tantangan yang kompleks dalam pengelolaan pergudangan hotel.Dengan memanfaatkan teknologi terkini, Tracklyn dapat membantu hotel meningkatkan efisiensi, mengurangi biaya, dan meningkatkan kualitas layanan. </p>
+                <a href="" class="card-button">Read More</a>
+            </div>
+        </div>
+        <div class="card-container">
+        <div class="card">
+            <img src="warehousing animation.jpg"  height="100" width="200">
+            <div class="card-content">
+                <h1>Tracklyn</h1>
+                <p>Dengan memanfaatkan teknologi terkini, Tracklyn dapat membantu hotel meningkatkan efisiensi, mengurangi biaya, dan meningkatkan kualitas layanan.</p>
+                <a href="" class="card-button">Read More</a>
+            </div> 
+        </div>
+        <div class="card-container">
+        <div class="card">
+            <img src="warehousing4.jpg"  height="100" width="200">
+            <div class="card-content">
+                <h1>Tracklyn</h1>
+                <p>Alasan kami mengangkat website ini dikarenakan Tracklyn dapat mengatasi masalah-masalah  umum yang dihadapi oleh hotel, seperti kekurangan stok, kerusakan barang, dan kesulitan dalam melacak barang.</p>
+                <a href="" class="card-button">Read More</a>
+            </div>
+        </div>
+        </div>
+         </div>
+    </body>
+</html>
